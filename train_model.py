03c44f72df1657a5cd5c7e0850f80e2d2176ac16
Your Name <you@example.com>
2026-03-14 02:15:32 +0100

    Fix duplicate train_model function

--- train_model.py
+++ train_model.py
@@ -1,14 +1,8 @@
 from sklearn.ensemble import RandomForestClassifier
 
-<<<<<<< HEAD
 #Entraîner le modèle
 def train_model(X_train, y_train):
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
     return model
-=======
-def train_model(X_train, y_train):
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
-    return model
->>>>>>> 6e3a2a09634f8f8a92c540cd44e21c494b9ab5f8
+
